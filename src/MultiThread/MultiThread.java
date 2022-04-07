1564f1ca91c6e9369455836e9b73eb0f381c34e7
Olegus85 <Olegus85@yandex.ru>
2022-04-07 17:53:43 +0300

    1 commit

---
+++ src/MultiThread/MultiThread.java
@@ -0,0 +1,29 @@
+package MultiThread;
+
+public class MultiThread extends Thread{
+    volatile static int i=0;
+
+    @Override
+    public void run() {
+        for (int t = 0; t < 20; t++) {
+            System.out.println("i= " + i);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    synchronized public static void main(String[] args) throws InterruptedException {
+        new MultiThread().start();
+        for(int t=0;t<20;t++){
+        i=t;
+        i=i+10;
+        System.out.println("from main i= "+i);
+        Thread.sleep(500);}
+    }
+}
+
+
+
